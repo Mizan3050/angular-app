da362f9501aff6bb1f74d863245442173b64a922
Meezan Shaikh <meezan@tradexa.co.in>
2024-07-14 18:55:46 +0530

    app component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  product$ = this.http.get(`https://fakestoreapi.com/products`).pipe(
+    tap(console.log)
+  )
+
 }
